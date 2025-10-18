595ffacb42506ffc562f373a98bfbabe156f7f05
Spider-Man-s <filip.posavec@fer.hr>
2025-10-18 13:04:13 +0200

    Added 2 houses and fire package

---
+++ Assets/Brick Project Studio/_BPS Basic Assets/Common/Scripts and Animations/First Person Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SojaExiles
+
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+
+        public CharacterController controller;
+
+        public float speed = 5f;
+        public float gravity = -15f;
+
+        Vector3 velocity;
+        Vector2 moveInput;
+
+        
+
+        
+        void Update()
+        {
+
+            Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+            controller.Move(move * speed * Time.deltaTime);
+
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+
+        }
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            moveInput = context.ReadValue<Vector2>();
+        }
+    }
+}
