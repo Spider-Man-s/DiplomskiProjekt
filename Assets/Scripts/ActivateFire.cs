5b3f2465b47413ea6b9e076560e0ae71f72d4bb5
Vilim Ivanković <117679646+VilimIv@users.noreply.github.com>
2025-11-29 16:19:35 +0100

    RADI MOLIM TE

--- Assets/Scripts/ActivateFire.cs
+++ Assets/Scripts/ActivateFire.cs
@@ -1,22 +1,37 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Photon.Pun;
+using ExitGames.Client.Photon;
+using Photon.Realtime;
 
 public class ActivateFire : MonoBehaviour
 {
-   [SerializeField] private List<GameObject> flames = new List<GameObject>();
+    [SerializeField] private List<GameObject> flames = new List<GameObject>();
 
-   
+    private const byte EVENT_FIRE_ACTIVATED = 1;
 
-    public void ActivateFlame(int flameID){
-        if(flames[flameID-1]!=null){
-            flames[flameID-1].SetActive(true);
+    public void ActivateFlame(int flameID)
+    {
+        if (flames[flameID - 1] != null)
+        {
+            flames[flameID - 1].SetActive(true);
+
+            string message = $"upaljen je požar broj {flameID}";
+            var raiseOptions = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+            PhotonNetwork.RaiseEvent(
+                EVENT_FIRE_ACTIVATED,
+                message,
+                raiseOptions,
+                SendOptions.SendReliable
+            );
         }
     }
 
-    public void DeActivateFlame(int flameID){
-        if(flames[flameID-1]!=null){
-            flames[flameID-1].SetActive(false);
+    public void DeActivateFlame(int flameID)
+    {
+        if (flames[flameID - 1] != null)
+        {
+            flames[flameID - 1].SetActive(false);
         }
     }
-
 }
